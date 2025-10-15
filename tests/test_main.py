eabe6f27e4edd0f6faf7d58cdc9b07913cdacd6d
S. M. Emzaz <sm.emzaz@enosisbd.com>
2025-10-15 16:51:37 +0600

    Add proper blank lines to avoid lint errors

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,10 +3,12 @@ from app.main import app
 
 client = TestClient(app)
 
+
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello from FastAPI!"}
+
 
 def test_read_item():
     response = client.get("/items/1?q=fastapi")
