867bd09b54077a53da63aa1919ba0fa1f3e6389c
S. M. Emzaz <sm.emzaz@enosisbd.com>
2025-10-15 12:11:35 +0600

    Initial commit

---
+++ tests/test_main.py
@@ -0,0 +1,14 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello from FastAPI!"}
+
+def test_read_item():
+    response = client.get("/items/1?q=fastapi")
+    assert response.status_code == 200
+    assert response.json() == {"item_id": 1, "q": "fastapi"}
